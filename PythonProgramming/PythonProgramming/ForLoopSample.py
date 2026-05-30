b73c52201c9bb83f7b52aaae03e4f54f45abe82b
chandra <jayacpc9@gmail.com>
2026-05-29 21:37:03 -0500

    daily updates

--- PythonProgramming/PythonProgramming/ForLoopSample.py
+++ PythonProgramming/PythonProgramming/ForLoopSample.py
@@ -1,2 +1,22 @@
-for i in range(10):
+limit =5
+
+for i in range(limit):
     print("index -> ",i)
+
+print("*"*50)
+for i in range(limit,0,-1):
+    print("Step -1 : reverse index -> ",i)
+
+print("*"*50)
+for i in range(0,limit,2):
+    print("Step 2 : index -> ",i)
+
+
+print("*"*50)
+for i in range(0,limit,3):
+    print("Step 3 : index -> ",i)
+
+
+print("*"*50)
+for i in range(0,limit):
+    print("No step : index -> ",i)
