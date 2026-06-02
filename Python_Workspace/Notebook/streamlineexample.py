dd2a690cfa8cfe94b281387207d42c172816e140
chandra <jayacpc9@gmail.com>
2026-06-02 00:22:33 -0500

    daily updates

---
+++ Python_Workspace/Notebook/streamlineexample.py
@@ -0,0 +1,10 @@
+
+import streamlit as st
+st.write("""# My first code
+         Welcome to the VS Code 
+         First streamlit app
+         Hello World!""")
+
+# run the code as 
+# streamlit run StreamlitTest.py 
+# streamlit run /Users/chandra/Desktop/FSDS_GenAI_Training/FSDS_Classes/Python_Workspace/Notebook/streamlineexample.py
