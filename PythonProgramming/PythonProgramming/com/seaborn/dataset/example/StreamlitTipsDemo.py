d5d3b13667102a7e5d1e6503abb340e0aa6655f6
chandra <jayacpc9@gmail.com>
2026-06-18 22:45:18 -0500

    daily updates

--- PythonProgramming/PythonProgramming/com/seaborn/dataset/example/StreamlitTipsDemo.py
+++ PythonProgramming/PythonProgramming/com/seaborn/dataset/example/StreamlitTipsDemo.py
@@ -23,38 +23,38 @@ graph_type = st.sidebar.selectbox(
     options=all_graph_types
 )
 selected_graph_index = all_graph_types.index(graph_type)
+if (selected_graph_index != 0):
+    with st.sidebar.form("sns_dataset_form"):
+        st.write("### Select your attributes to generate a graph:")
 
-with st.sidebar.form("sns_dataset_form"):
-    st.write("### Select your attributes to generate a graph:")
-
-    x_axis = st.selectbox(
-        label="X-axis attribute:",
-        options=all_columns
-    )
-    if (selected_graph_index != 1):
-        y_axis = st.selectbox(
-            label="Y-axis attribute:",
+        x_axis = st.selectbox(
+            label="X-axis attribute:",
             options=all_columns
         )
+        if (selected_graph_index != 1):
+            y_axis = st.selectbox(
+                label="Y-axis attribute:",
+                options=all_columns
+            )
 
-        hue_val = st.selectbox(
-            label="Hue : ",
-            options=all_columns
-        )
+            hue_val = st.selectbox(
+                label="Hue : ",
+                options=all_columns
+            )
 
 
-    submitted = st.form_submit_button("Generate Graph")
-log_message =f"**Selected Items:**"
-if submitted:
-    st.success(f"Form submitted successfully!")
-    match selected_graph_index:
-        case 1:
-            log_message+=f"x = {x_axis} "
-            display_plot("Histogram plot ", lambda: tips.hist_plot(ax, x_axis))
-        case 2:
-            log_message+=f"x = {x_axis} , y = {y_axis} , hue = {hue_val} "
-            display_plot("scatter plot ", lambda: tips.scatter_plot(ax, x_axis, y_axis, hue_val))
-        case 3:
-            log_message+=f"x = {x_axis} , y = {y_axis} , hue = {hue_val} "
-            display_plot("Box plot ", lambda: tips.box_plot(ax, x_axis, y_axis, hue_val))
-    st.write(log_message)
+        submitted = st.form_submit_button("Generate Graph")
+    log_message =f"**Selected Items:**"
+    if submitted:
+        st.success(f"Form submitted successfully!")
+        match selected_graph_index:
+            case 1:
+                log_message+=f"x = {x_axis} "
+                display_plot("Histogram plot ", lambda: tips.hist_plot(ax, x_axis))
+            case 2:
+                log_message+=f"x = {x_axis} , y = {y_axis} , hue = {hue_val} "
+                display_plot("scatter plot ", lambda: tips.scatter_plot(ax, x_axis, y_axis, hue_val))
+            case 3:
+                log_message+=f"x = {x_axis} , y = {y_axis} , hue = {hue_val} "
+                display_plot("Box plot ", lambda: tips.box_plot(ax, x_axis, y_axis, hue_val))
+        st.write(log_message)
