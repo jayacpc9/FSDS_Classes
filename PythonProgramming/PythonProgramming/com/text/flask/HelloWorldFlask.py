ceaa49c3255fd17803401c937b535087b3563475
chandra <jayacpc9@gmail.com>
2026-07-04 00:45:44 -0500

    update

--- PythonProgramming/PythonProgramming/com/text/flask/HelloWorldFlask.py
+++ PythonProgramming/PythonProgramming/com/text/flask/HelloWorldFlask.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return "<H1>Hello, World..!!</H1>"
 
-# app.run(host='192.168.1.65', port=81)
+
+if __name__ == "__main__":
+    # host_ip='127.0.0.1'
+    host_ip='192.168.1.65'
+    host_port =8085
+    app.run(host=host_ip, port=host_port)
