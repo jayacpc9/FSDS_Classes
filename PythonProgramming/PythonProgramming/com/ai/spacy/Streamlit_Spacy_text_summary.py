e9dc1306798878f19d4979af0d01f4b293156dc0
chandra <jayacpc9@gmail.com>
2026-08-09 22:37:45 -0500

    daily updates

--- PythonProgramming/PythonProgramming/com/ai/spacy/Streamlit_Spacy_text_summary.py
+++ PythonProgramming/PythonProgramming/com/ai/spacy/Streamlit_Spacy_text_summary.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import spacy_text_summary as sp_text
-default_text =''' There are broadly two types of extractive summarization tasks depending on what the summarization program focuses on. The first is generic summarization, which focuses on obtaining a generic summary or abstract of the collection (whether documents, or sets of images, or videos, news stories etc.). The second is query relevant summarization, sometimes called query-based summarization, which summarizes objects specific to a query. Summarization systems are able to create both query relevant text summaries and generic machine-generated summaries depending on what the user needs.'''
+default_text ='''Enter your text here.'''
 st.title("Spacy Text Summarization")
 st.header("Enter your text and generate summary using Spacy.")
 user_text = st.text_area("Enter some text here:", key="user_text_area",value=default_text)
